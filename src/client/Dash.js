70167d5c355323580c086801a477758ed04f20f5
Pranav Sindura <pranav.sindura@gmail.com>
2020-02-04 02:44:17 +0530

    user

--- src/client/Dash.js
+++ src/client/Dash.js
@@ -2,8 +2,9 @@ import React, { Component } from 'react';
 import { Button, Table } from 'react-bootstrap';
 import axios from 'axios';
 export default class Dash extends Component {
-	state = { option: 0, eventSelected: '', eventArray: [], userSearchID: '', userArray: [] };
-	proxy = 'http://localhost:8080';
+	state = { option: 0, eventSelected: '', eventArray: [], userSearchID: '', userObj: {} };
+	// proxy = 'http://localhost:8080';
+	proxy = '';
 	componentDidMount() {
 		console.log(this.props);
 		this.setState({ eventSelected: this.props.eventID[0] });
@@ -103,17 +104,123 @@ export default class Dash extends Component {
 	handleUserChange(e) {
 		this.setState({ userSearchID: e.target.value });
     }
-    renderTableUser(arr)
+    makeHospitalityOptions(obj)
     {
-        return arr;
+        if(!obj.hospitality)
+            return '';
+        let opt = '';
+        obj.hospitality.sort();
+        for(let i=0;i<obj.hospitality.length;i++)
+        {
+                opt += obj.hospitality[i] + ' ';
+        }
+        return opt;
     }
-    handleUserSubmit(e)
-    {
-        e.preventDefault();
-        console.log(this.state.userSearchID);
-    }
+	makeParticipateList(obj) {
+		if (!obj.events) return null;
+
+		let ev = [];
+		for (let i = 0; i < obj.events.length; i++)
+			ev.push(
+				<li>
+					{obj.events[i].name}
+					<br /> ID: {obj.events[i].teamID}
+					<br /> Team Name: {!obj.events[i].teamName.length ? 'N/A' : obj.events[i].teamName}
+					<br /> Team Size: {obj.events[i].teamSize}
+				</li>
+			);
+		return <ol>{ev}</ol>;
+	}
+	renderTableUser(obj) {
+		return (
+			<Table bordered responsive>
+				<thead></thead>
+				<tbody>
+					<tr>
+						<th>Blitz ID</th>
+						<td>{obj.blitzID}</td>
+					</tr>
+					<tr>
+						<th>First Name</th>
+						<td>{obj.firstName}</td>
+					</tr>
+					<tr>
+						<th>Last Name</th>
+						<td>{obj.lastName}</td>
+					</tr>
+					<tr>
+						<th>Gender</th>
+						<td>{obj.gender}</td>
+					</tr>
+					<tr>
+						<th>Email</th>
+						<td>{obj.email}</td>
+					</tr>
+					<tr>
+						<th>Mobile</th>
+						<td>{obj.mob}</td>
+					</tr>
+					<tr>
+						<th>Course</th>
+						<td>{obj.course}</td>
+					</tr>
+					<tr>
+						<th>Year</th>
+						<td>{obj.year}</td>
+					</tr>
+					<tr>
+						<th>Branch</th>
+						<td>{obj.branch}</td>
+					</tr>
+					<tr>
+						<th>City</th>
+						<td>{obj.city}</td>
+					</tr>
+					<tr>
+						<th>College</th>
+						<td>{obj.college}</td>
+					</tr>
+					<tr>
+						<th>College ID</th>
+						<td>{obj.collegeID}</td>
+					</tr>
+					<tr>
+						<th>Accomodation</th>
+						<td>{obj.accomodation ? 'YES' : 'NO'}</td>
+					</tr>
+					<tr>
+						<th>Participated Events</th>
+						<td>{this.makeParticipateList(obj)}</td>
+					</tr>
+					<tr>
+						<th>Hospitality Options</th>
+						<td>{this.makeHospitalityOptions(obj)}</td>
+					</tr>
+				</tbody>
+			</Table>
+		);
+	}
+	handleUserSubmit(e) {
+		e.preventDefault();
+		// console.log(this.state.userSearchID);
+		const { userSearchID } = this.state;
+		axios
+			.post(this.proxy + '/user', { blitzID: userSearchID })
+			.then((res) => {
+				res = res.data;
+				console.log(res);
+				if (res.status) {
+					this.setState({ userObj: res.message });
+				} else {
+					alert('Invalid ID!');
+				}
+			})
+			.catch((e) => {
+				console.log(e);
+			});
+	}
 	viewUser() {
-		const { userSearchID, userArray } = this.state;
+		const { userSearchID, userObj } = this.state;
 		return (
 			<div>
 				<br />
@@ -134,7 +241,7 @@ export default class Dash extends Component {
 					/>
 					<button type="submit">Submit</button>
 				</form>
-				<div>{this.renderTableUser(userArray)}</div>
+				<div>{this.renderTableUser(userObj)}</div>
 			</div>
 		);
 	}
@@ -156,7 +263,7 @@ export default class Dash extends Component {
 		}
 	}
 	selectOption(option) {
-		this.setState({ option, eventArray: [], userSearchID: '', userArray: [] });
+		this.setState({ option, eventArray: [], userSearchID: '', userObj: [] });
 	}
 	render() {
 		return (
