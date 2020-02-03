4b5f9731075037f3af6100dfdc0e84b5d1a78159
Dhairya-patel <dhairya.patel0@gmail.com>
2020-02-03 23:31:39 +0530

    added user and accomodation route with paymodel for approval

--- src/server/moderator.js
+++ src/server/moderator.js
@@ -68,7 +68,89 @@ async function findUsersOfEvent(id) {
     return result;
 }
 
+async function retrieveUser(id) {
+    let user = await userModel.findOne({ blitzID: id }, (err, result) => {
+        if (err) {
+            console.log('ID not found');
+        }
+    });
+    return user;
+}
+
+async function findMaleAcc() {
+    let result = await userModel.find({ gender: 'Male', accomodation: true }, (err, result) => {
+        if (err) {
+            console.log('error');
+        }
+    });
+    for (user of result) {
+        user.blitzPIN = undefined;
+        user.hospitality = undefined;
+        user.paymentHistory = undefined;
+        user.gender = undefined;
+        user.collegeID = undefined;
+        user.year = undefined;
+        user.course = undefined;
+        user.branch = undefined;
+        user.isMNIT = undefined;
+        user.accomodation = undefined;
+        user.events = undefined;
+        user.__v = undefined;
+    }
+    return result;
+}
+
+async function findFemaleAcc() {
+    let result = await userModel.find({ gender: 'Female', accomodation: true }, (err, result) => {
+        if (err) {
+            console.log('error');
+        }
+    });
+    for (user of result) {
+        user.blitzPIN = undefined;
+        user.hospitality = undefined;
+        user.paymentHistory = undefined;
+        user.gender = undefined;
+        user.collegeID = undefined;
+        user.year = undefined;
+        user.course = undefined;
+        user.branch = undefined;
+        user.isMNIT = undefined;
+        user.accomodation = undefined;
+        user.events = undefined;
+        user.__v = undefined;
+    }
+    return result;
+}
+
+async function findOthersAcc() {
+    let result = await userModel.find({ gender: 'Others', accomodation: true }, (err, result) => {
+        if (err) {
+            console.log('error');
+        }
+    });
+    for (user of result) {
+        user.blitzPIN = undefined;
+        user.hospitality = undefined;
+        user.paymentHistory = undefined;
+        user.gender = undefined;
+        user.collegeID = undefined;
+        user.year = undefined;
+        user.course = undefined;
+        user.branch = undefined;
+        user.isMNIT = undefined;
+        user.accomodation = undefined;
+        user.events = undefined;
+        user.__v = undefined;
+    }
+    return result;
+}
+
 module.exports = {
     validateMod,
-    findUsersOfEvent
+    findUsersOfEvent,
+    retrieveUser,
+    findMaleAcc,
+    findFemaleAcc,
+    findOthersAcc
 }
